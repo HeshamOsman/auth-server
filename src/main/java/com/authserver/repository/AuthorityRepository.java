da402893fb567db186cba9f9c7a5f4b54c16cbaf
HeshamOsman <hesham.osman28@yahoo.com>
2019-07-18 02:10:58 -0700

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/com/authserver/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.authserver.repository;
+
+import com.authserver.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
